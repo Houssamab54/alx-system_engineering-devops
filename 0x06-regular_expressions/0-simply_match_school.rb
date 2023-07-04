d1a5803823502c4a2f70306faef8848dcf75eebf
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-07-04 06:55:30 -0700

    ggg

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-regex = /School/
-input_string = ARGV[0]
-
-if input_string =~ regex
-  puts regex.match(input_string)
-else
-  puts ""
-end
+puts ARGV[0].scan(/Holberton/).join
